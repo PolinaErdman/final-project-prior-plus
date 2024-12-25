fa62ba95bb92d181d3c6f093917ecae3e1957ec4
palinaerdman <polinaerdman@gmail.com>
2024-12-25 18:30:46 +0300

    refactor classes

--- src/test/java/by/plus/priorbank/api/LoginTest.java
+++ src/test/java/by/plus/priorbank/api/LoginTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
-public class LoginApiTest extends BaseTest{
+public class LoginTest extends BaseTest{
 
     @Test
     @DisplayName("Авторизация c невалидными данными")
